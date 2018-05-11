71f947300809d142a4ebe25f6e0473a168c4e57c
nqmt <to.nqmt@gmail.com>
2018-05-12 01:18:56 +0700

    example 2

--- webworker/src/scripts/main.js
+++ webworker/src/scripts/main.js
@@ -1,21 +1,16 @@
 var workerFor = new Worker('../workers/for.js');
+var blob = new ArrayBuffer(100);
+var blobObj = {img1: new ArrayBuffer(100), img2: new ArrayBuffer(300)};
+var blobArr = [new ArrayBuffer(100), new ArrayBuffer(200)];
+
 // listen to message event of worker
 workerFor.onmessage = function(event){
-    var div = document.getElementById('result');
-    div.innerHTML = 'message received => ' + event.data;
-    workerFor.terminate();
-};
-// listen to error event of worker
-workerFor.onerror = function(event) {
-    console.error('error received from workerFor => ', event);
-    var div = document.getElementById('result');
-    div.innerHTML = 'Error!';
+    console.log("[onmessage] blob = ", blob);
 };
 // load results from web worker
 function loadResult() {
-    // add loading text until `message` event listener replaces it
-    var div = document.getElementById('result');
-    div.innerHTML = 'loading...';
-    // emit message event to worker
-    workerFor.postMessage(null); // we don't need payload here
+    console.log("[loadResult] blob = ", blob);
+    workerFor.postMessage(blob, [blob]);
+    workerFor.postMessage(blobObj, [blobObj.img1]); // transfer only blobObj.img1
+    workerFor.postMessage(blobArr, [blobArr[1]]); // transfer only blobObj[1]
 };
--- webworker/src/workers/for.js
+++ webworker/src/workers/for.js
@@ -1,9 +1,4 @@
-// for.js
 self.onmessage = function(event) {
-    var x = 0;
-    for (var i = 0; i < 200000000; i++) {
-        x = x + i;
-    }
-    self.postMessage(x);
-    self.close();
+    console.log("[for.js] blob = ", event.data);
+    self.postMessage("SAMPLE_PROCESSED_DATA");
 }
